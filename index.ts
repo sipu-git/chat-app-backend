35acea46e803582cb3d2fe0d48ddf92e1a25557b
Sipu-982 <sipurana38@gmail.com>
2026-01-09 11:59:57 +0530

    main file updated

--- index.ts
+++ index.ts
@@ -14,19 +14,20 @@ const app = express();
 const server = http.createServer(app);
 
 app.use(cors({
-    origin: ['http://ec2-13-233-23-20.ap-south-1.compute.amazonaws.com','https://chat-app-frontend-sooty-six.vercel.app'],
+    origin: ['http://ec2-13-233-23-20.ap-south-1.compute.amazonaws.com', 'https://chat-app-frontend-sooty-six.vercel.app'],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(passport.initialize())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 connectDB();
 
 app.use('/api/users', userRoutes);
-app.use('/api/auth',authRoutes);
-app.use('/api/chats',chatRoutes)
+app.use('/api/auth', authRoutes);
+app.use('/api/chats', chatRoutes)
 const PORT = process.env.PORT || 4000;
 initSocket(server)
 server.listen(PORT, () => console.log(`Server and socket running on ${PORT}`));
